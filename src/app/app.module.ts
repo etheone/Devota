a6f3b23202bd0c4472c21f3d25f502b1be3b8cdb
Emil <emini901@student.liu.se>
2017-01-20 14:49:02 +0100

    Added all (for now) components and configured routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,52 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DevicesComponent } from './devices/devices.component';
+import { DataComponent } from './data/data.component';
+import { GroupsComponent } from './groups/groups.component';
+
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+    {
+    path: 'devices',
+    component: DevicesComponent
+  },
+    {
+    path: 'data',
+    component: DataComponent
+  },
+    {
+    path: 'groups',
+    component: GroupsComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DevicesComponent,
+    DataComponent,
+    GroupsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
